0e39670f9debb393512b673c2365530d8b12310e
Abhijith PK <pkabhijith1@gmail.com>
2023-08-14 15:38:20 +0530

    update and delete products done

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ import CreateProduct from './pages/admin/CreateProduct';
 import Users from './pages/admin/Users';
 import Orders from './pages/user/Orders';
 import Profile from './pages/user/Profile';
+import Products from './pages/admin/Products';
+import UpdateProject from './pages/admin/UpdateProject';
 
 
 function App() {
@@ -37,6 +39,8 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct/>} />
+          <Route path="admin/product/:slug" element={<UpdateProject/>} />
+          <Route path="admin/products" element={<Products/>} />
           <Route path="admin/users" element={<Users/>} />
         </Route>
 
